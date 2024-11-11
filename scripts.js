e4d8e4f63ae06ee894be2671310a1ed5bf2d1cb6
DowloadQRcode <qrtactico@gmail.com>
2024-11-11 19:09:32 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -21,7 +21,19 @@ document.getElementById('qr-form').addEventListener('submit', function(e) {
     }
 
     QRCode.toCanvas(data, { width: 200, height: 200, color: { dark: '#000000', light: '#ffffff' }}, function(error, canvas) {
-        if (error) console.error(error);
+        if (error) {
+            console.error(error);
+            return;
+        }
+
+        // Añadir el canvas con el QR generado al contenedor
         qrCodeContainer.appendChild(canvas);
+
+        // Hacer visible el botón de descarga
+        const downloadButton = document.getElementById('download-btn');
+        downloadButton.style.display = 'inline-block';
+
+        // Funcionalidad de descarga
+        downloadButton.href = canvas.toDataURL('image/png');
     });
 });
